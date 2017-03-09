57da8d95ffe84f629611707c1b632ea90fa28fad
Sangle <whb97@163.com>
2017-03-08 19:04:26 -0600

    Add files via upload

---
+++ musicplayer/playlist.js
@@ -0,0 +1,61 @@
+let data = [{
+    index: 1,
+    name: 'A Lovely Night',
+    artist: 'Emma Stone & Ryan Gosling',
+    url: 'http://link.hhtjim.com/163/445867335.mp3',
+    cover: 'img/timg.jpg'
+}, {
+    index: 2,
+    name: '二人セゾン',
+    artist: '欅坂46',
+    url: 'http://link.hhtjim.com/163/443882735.mp3',
+    cover: 'img/ftr.jpg'
+}, {
+    index: 3,
+    name: 'City Of Stars',
+    artist: 'Emma Stone & Ryan Gosling',
+    url: 'http://link.hhtjim.com/163/441612583.mp3',
+    cover: 'img/timg.jpg'
+}, {
+    index: 4,
+    name: 'Audition (The Fools Who Dream)',
+    artist: 'Emma Stone',
+    url: 'http://link.hhtjim.com/163/445867343.mp3',
+    cover: 'img/timg.jpg'
+}, {
+    index: 5,
+    name: 'Stronger',
+    artist: 'Kelly Clarkson',
+    url: 'http://link.hhtjim.com/163/26569168.mp3',
+    cover: 'img/stronger.jpg'
+}, {
+    index: 6,
+    name: '命は美しい',
+    artist: '乃木坂46',
+    url: 'http://link.hhtjim.com/163/414691362.mp3',
+    cover: 'img/ino.jpg'
+}, {
+    index: 7,
+    name: 'Epilogue',
+    artist: 'Justin Hurwitz',
+    url: 'http://link.hhtjim.com/163/445867344.mp3',
+    cover: 'img/timg.jpg'
+}, {
+    index: 8,
+    name: 'Call Me Maybe',
+    artist: 'Carly Rae Jepsen',
+    url: 'http://link.hhtjim.com/163/17112299.mp3',
+    cover: 'img/cmm.jpg'
+}, {
+    index: 9,
+    name: 'Start A Fire',
+    artist: 'John Legend',
+    url: 'http://link.hhtjim.com/163/445867341.mp3',
+    cover: 'img/timg.jpg'
+}, {
+    index: 10,
+    name: '小半',
+    artist: '陈粒',
+    url: 'http://link.hhtjim.com/163/421423806.mp3',
+    cover: 'img/xmdb.jpg'
+}]
